c1ece8d55046d68a499a92b0cc6b41bf4a39d883
Maria Viana do Nascimento <mariaviana05091997@gmail.com>
2025-02-27 22:31:45 -0300

    Implementando o style.css nas paginas e o js.

---
+++ script.js
@@ -0,0 +1,7 @@
+function responderCorretamente() {
+    alert("Ótima escolha! Segurança em primeiro lugar.");
+}
+
+function redirecionarParaSimulacao() {
+    window.location.href = "simulacao.html";
+}
